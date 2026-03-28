8667dab2405cedabd39537406c22a3898e762be1
Macovei David <64650243+snappi13@users.noreply.github.com>
2026-03-28 12:27:25 +0000

    1

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "YOUR_BUCKET_NAME"
+    bucket  = "davidstefanfiipracticawscloud"
     key     = "terraform.tfstate"
     region  = "eu-north-1"
     encrypt = true
